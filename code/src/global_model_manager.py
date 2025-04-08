7653cf71d4b95790c7949c6ece5242f479a7e36d
benatkinsonx <142601279+benatkinsonx@users.noreply.github.com>
2025-04-08 13:54:15 +0100

    starting global model

---
+++ code/src/global_model_manager.py
@@ -0,0 +1,12 @@
+import pickle
+import pandas as pd
+import numpy as np
+from pyspark.sql import DataFrame
+from aeon.classification.distance_based import ProximityTree, ProximityForest
+import logging
+
+class GlobalModelManager:
+    def __init__(self):
+        pass
+
+    
